be82405ab4904fbf09a8da4ac72c73bb4d250939
Tika299 <vukofa9120@gmail.com>
2025-11-06 14:46:12 +0700

    c

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',               // <-- THÊM DÒNG NÀY
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
 })
